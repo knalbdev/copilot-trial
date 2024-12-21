286eb5cdbc9966a6bce6201162038c48a00ac219
Setianing Budi <setianingbudi2@gmail.com>
2024-12-21 22:26:03 +0700

    change stateless into stateful

--- lib/sample_page.dart
+++ lib/sample_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class SamplePage extends StatelessWidget {
+class SamplePage extends StatefulWidget {
   const SamplePage({super.key});
 
+  @override
+  State<SamplePage> createState() => _SamplePageState();
+}
+
+class _SamplePageState extends State<SamplePage> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
